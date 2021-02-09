abbfdd20b59bcc5a2d1608039063f50e32612f51
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-02-09 14:11:02 -0300

    done with non blocking restful api

---
+++ mono_flux/src/test/java/com/gianvittorio/springreactive/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,120 @@
+package com.gianvittorio.springreactive.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+public class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void fluxApproach1() {
+        Flux<Integer> integerFlux = webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux.log())
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectComplete();
+    }
+
+    @Test
+    public void fluxApproach2() {
+        webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void fluxApproach3() {
+        List<Integer> expectedIntegerList = Arrays.asList(1, 2, 3, 4);
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertThat(entityExchangeResult.getResponseBody())
+                .isEqualTo(expectedIntegerList);
+    }
+
+    @Test
+    public void fluxApproach4() {
+        List<Integer> expectedIntegerList = Arrays.asList(1, 2, 3, 4);
+
+        webTestClient.get()
+                .uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith(response -> {
+                    assertThat(response.getResponseBody())
+                            .isEqualTo(expectedIntegerList);
+                });
+    }
+
+    @Test
+    public void fluxStream() {
+        Flux<Integer> integerFlux = webTestClient.get()
+                .uri("/fluxstream")
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectNext(0)
+                .expectNext(1)
+                .expectNext(2)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    public void mono() {
+        webTestClient.get()
+                .uri("/mono")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(consumer -> {
+                    assertThat(consumer.getResponseBody())
+                            .isEqualTo(1);
+                });
+    }
+}
