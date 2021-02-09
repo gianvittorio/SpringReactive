588011ceb211acc6b9dc1810c676551b6d4a438c
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-02-09 15:29:01 -0300

    done with non blocking restful api functional

---
+++ mono_flux/src/test/java/com/gianvittorio/springreactive/handler/SamplehandlerFunctionTest.java
@@ -0,0 +1,55 @@
+package com.gianvittorio.springreactive.handler;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SamplehandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void functionalFluxTest() {
+        Flux<Integer> integerFlux = webTestClient.get()
+                .uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void functionalMonoTest() {
+        webTestClient.get()
+                .uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(
+                        consumer -> assertThat(consumer.getResponseBody()).isEqualTo(1)
+                );
+    }
+}
